8f85a16cfb8bc54c2be579ab5c121064d56b9aeb
Gabriel Silva <gabriel127.gk@gmail.com>
2022-12-27 18:37:06 -0300

    .

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   swcMinify: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "http://localhost:3000",
   },
   images: {
     domains: ["localhost"],
