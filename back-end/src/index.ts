d47242369e5dd67bc53d7e776e3b00b5c4072bb7
Gabriel Silva <gabriel127.gk@gmail.com>
2023-02-16 15:11:15 -0300

    Deploy back-end na vercel - config. - 7

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,13 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 import { app } from "./server";
-
 import "express-async-errors";
 
 import { usersRoutes } from "./routes/users-routes";
 import { transactionsRoutes } from "./routes/transactions-routes";
-
 import { authMiddleware } from "./middlewares/auth-middleware";
-
 import { CustomError } from "./errors/CustomError";
 
 app.use("/users", usersRoutes);
