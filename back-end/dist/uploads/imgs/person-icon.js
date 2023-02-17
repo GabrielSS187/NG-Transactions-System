88f2ec8b63fa114a44681606a3bd2a4961f7b334
Gabriel Silva <gabriel127.gk@gmail.com>
2023-02-17 15:48:08 -0300

    deploy ciclyc - 1

--- back-end/dist/uploads/imgs/person-icon.js
+++
@@ -1,4 +0,0 @@
-// src/uploads/imgs/person-icon.png
-var person_icon_default = "../../person-icon-K6FO66GN.png";
-// Annotate the CommonJS export names for ESM import in node:
-0 && (module.exports = {});
