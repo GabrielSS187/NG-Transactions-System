0e7c47a5ad8236d8d496cebaac741b48b5628331
Gabriel Silva <gabriel127.gk@gmail.com>
2023-02-17 17:06:05 -0300

    deploy ciclyc - 11

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -11,6 +11,9 @@ import { CustomError } from "./errors/CustomError";
 app.use("/files", express.static(path.resolve("src/uploads/imgs")));
 app.use("/users", usersRoutes);
 app.use("/transactions", authMiddleware, transactionsRoutes);
+app.get("/", async (req: Request, res: Response) => {
+  return res.status(200).send("Testeeee!!!");
+})
 
 //* ============================================================
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
